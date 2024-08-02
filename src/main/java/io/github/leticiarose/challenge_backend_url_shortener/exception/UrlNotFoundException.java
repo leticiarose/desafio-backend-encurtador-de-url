aa7e9b522a8770c619dfca6292347066a83ab9f3
leticia.lopes <leticia.rosedesantana@gmail.com>
2024-08-02 15:56:15 -0300

    feat: create custom UrlNotFound Exception

---
+++ src/main/java/io/github/leticiarose/challenge_backend_url_shortener/exception/UrlNotFoundException.java
@@ -0,0 +1,45 @@
+package io.github.leticiarose.challenge_backend_url_shortener.exception;
+
+public class UrlNotFoundException extends RuntimeException {
+    private final int errorCode;
+
+    public UrlNotFoundException() {
+        super();
+        this.errorCode = 0;
+    }
+
+    public UrlNotFoundException(String message) {
+        super(message);
+        this.errorCode = 0;
+    }
+
+    public UrlNotFoundException(String message, int errorCode) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+
+    public UrlNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+        this.errorCode = 0;
+    }
+
+    public UrlNotFoundException(Throwable cause) {
+        super(cause);
+        this.errorCode = 0;
+    }
+
+    public UrlNotFoundException(Throwable cause, int errorCode) {
+        super(cause);
+        this.errorCode = errorCode;
+    }
+
+    public UrlNotFoundException(String message, Throwable cause, int errorCode) {
+        super(message, cause);
+        this.errorCode = errorCode;
+    }
+
+    public int getErrorCode() {
+        return errorCode;
+    }
+
+}
