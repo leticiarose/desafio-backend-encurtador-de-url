8f261445879b878711d2b050f4f2973fa6930e84
leticia.lopes <leticia.rosedesantana@gmail.com>
2024-08-02 15:36:43 -0300

    feat: initial project setup and creation

---
+++ src/test/java/io/github/leticiarose/challenge_backend_url_shortener/ChallengeBackendUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leticiarose.challenge_backend_url_shortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeBackendUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
