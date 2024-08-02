e2e893bd97b2a19cdcf3293f3efd24dc8102a373
leticia.lopes <leticia.rosedesantana@gmail.com>
2024-08-02 15:47:21 -0300

    feat: create UrlShort DTO

---
+++ src/main/java/io/github/leticiarose/challenge_backend_url_shortener/dto/UrlShortDTO.java
@@ -0,0 +1,14 @@
+package io.github.leticiarose.challenge_backend_url_shortener.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class UrlShortDTO {
+
+    private String urlShort;
+
+}
