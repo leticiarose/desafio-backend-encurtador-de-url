3262f96454c8f0568d6905033a7432ddd97b1180
leticia.lopes <leticia.rosedesantana@gmail.com>
2024-08-02 15:41:46 -0300

    feat: create Url entity

---
+++ src/main/java/io/github/leticiarose/challenge_backend_url_shortener/model/Url.java
@@ -0,0 +1,33 @@
+package io.github.leticiarose.challenge_backend_url_shortener.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity(name = "urls")
+public class Url {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String urlComplete;
+    private String urlShort;
+    private Long accessCount = 0L;
+    private LocalDate dateFirstAccess;
+    private Double dailyAccessAverage = 0.0;
+
+    public Url(String urlComplete, String urlShort) {
+        this.urlComplete = urlComplete;
+        this.urlShort = urlShort;
+    }
+
+}
