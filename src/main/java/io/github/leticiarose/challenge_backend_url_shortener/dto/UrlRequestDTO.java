e1b6dc44c49add22dbbe935babacf5755f144518
leticia.lopes <leticia.rosedesantana@gmail.com>
2024-08-02 15:48:12 -0300

    feat: create UrlRequest DTO

---
+++ src/main/java/io/github/leticiarose/challenge_backend_url_shortener/dto/UrlRequestDTO.java
@@ -0,0 +1,12 @@
+package io.github.leticiarose.challenge_backend_url_shortener.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UrlRequestDTO {
+
+    private String urlComplete;
+
+}
